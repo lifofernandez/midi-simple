d68084b34484bbf7402512a7360a1849ab3be1cc
lifofernandez <lifofernandez@gmail.com>
2017-02-05 16:55:55 +0100

    bye ::Simple

--- gen_serie.pl
+++ gen_serie.pl
@@ -1,30 +1,47 @@
 
 use feature 'say';
 use Data::Dump qw( dump );
-use MIDI::Simple;
+use MIDI;
 
-new_score;
+# new_score;
 # set: chanel=1 to patch=1 (a piano)
-patch_change 1, 1;
+# patch_change 1, 1;
+
 
 my $octave = 3;
 
 my @chromatic = ( 0 .. 11 );
-my @diatonic = ( "A".."G" );
 my @mayor = ( 0, 2, 4, 5, 7, 9, 11 );
 my @minor = ( 0, 2, 3, 5, 7, 8, 10 );
 
-my @transp = map { $_ + ( 12 * $octave ) } @mayor;
+my @transp = map { $_ + ( 12 * $octave ) } @chromatic;
+dump( @transp );
 
 # shuffle :)
 my %pitches = @transp;  
 dump( %pitches );
 
 # setup empty note
-noop c1, f, hn, "o".$octave; 
-
-for my $pitch ( %pitches ){
-     n $pitch, "o".$octave;
+my @events = (
+    ['text_event',0, 'MORE KLAVIER'],
+    ['set_tempo', 0, 450_000], # 1qn = .45 seconds
+);
+ 
+for my $pitch ( sort {$a <=> $b} %pitches ){
+    say $pitch;
+    push @events,
+         ['note_on' , 90, 4, $pitch, 127],
+         ['note_off',  6, 4, $pitch, 127],
+    ;
 }
 
-write_score 'serie.mid';
+my $klavier_track= MIDI::Track->new({ 
+    'events' => \@events 
+});
+my $opus = MIDI::Opus->new({
+    'format' => 0, 
+    'ticks' => 96, 
+    'tracks' => [ $klavier_track] 
+});
+
+$opus->write_to_file( 'serie.mid' );
