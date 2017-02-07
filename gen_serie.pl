c7c750cff26ce8d92aac20f45bfada68de52ecfd
lifofernandez <lifofernandez@gmail.com>
2017-02-07 21:28:52 +0100

    regular commit

--- gen_serie.pl
+++ gen_serie.pl
@@ -42,16 +42,16 @@ my @secuencia = ( 1..12 );
 
 # Midi Setup
 my @events = (
-    #['set_tempo', 0, 450_000], # 1qn = .45 seconds
+    ['set_tempo', 0, 450_000], # 1qn = .45 seconds
 );
 
 for my $delta ( reverse @secuencia ){
     my $altura = @alturas[ ( $delta - 1) % $cantidad_alturas ] ;
-    my $duracion = 128 * @duraciones[ ( $delta - 1) % $cantidad_duraciones ] ;
+    my $duracion = 96 * @duraciones[ ( $delta - 1) % $cantidad_duraciones ] ;
     say $altura;
 
     push @events,
-       ['note_on' ,  1, 1, $altura, 127],
+       ['note_on' , $duracion / 2 , 1, $altura, 127],
        ['note_off', $duracion, 1, $altura, 127],
     ;
 }
@@ -62,8 +62,7 @@ my $klavier_track = MIDI::Track->new({
 });
 
 my $opus = MIDI::Opus->new({
-    'format' => 2, 
-    'ticks' => 128, 
+    'format' => 1, 
     'tracks' => [ $klavier_track] 
 });
 
