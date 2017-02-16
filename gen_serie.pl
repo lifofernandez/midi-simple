f08d17a03eba1fe809106f33d1455476d4b83d18
lifofernandez <lifofernandez@gmail.com>
2017-02-16 20:27:35 +0100

    regular commit

--- gen_serie.pl
+++ gen_serie.pl
@@ -2,56 +2,71 @@
 
 use feature 'say';
 use strict;
-use Data::Dump qw( dump );
+use Data::Dumper;
 use MIDI;
+use YAML::XS 'LoadFile';
+
+########################################
+# General setup
+
+my $pulso = 1000_000; # milisecs 
+
+
+########################################
+# Track config from YAML
+my $track_config = LoadFile( 'tracks/bajo.yaml' );
+
+my $canal = $track_config->{ canal };
 
 
 ########################################
 # Alturas
 
-my $tonica = 60;
-my $octave = -1;
+my $tonica = $track_config->{ tonica };
+my $octava = $track_config->{ octava };
+my @escala = @{ $track_config->{ escala }{ alturas } };
 
-my @chrom = ( 0 .. 12 );
-my @mayor = ( 0, 2, 4, 5, 7, 9, 11 );
-my @minor = ( 0, 2, 3, 5, 7, 8, 10, 12 );
-my @penta = ( 0, 2, 3, 4, 5 );
-
-my @alturas = map { 
-    $_ + $tonica + ( 12 * $octave ) 
-} @mayor;
+my @alturas = map { $_ + $tonica + ( 12 * $octava ) 
+} @escala;
 my $cantidad_alturas = scalar @alturas;
 
 
 ########################################
-# Duraciones
+# Duracion
 
-my @d1 = ( .5, .5, 1, 1, 1, 2 );
-my @d2 = ( .5, .5, 1, 2, .5, .5, 1, 2 );
-my @d2 = ( .5, .5, 1, 2, .5, .5, 1, 2 );
-
-my @duraciones;
-push @duraciones, @d1, @d1, @d2, @d1, 4;
+my @duraciones; 
+push ( @duraciones, ( "1" ) );
 my $cantidad_duraciones = scalar @duraciones;
+my $retardo = 10; #revisar esto 
+
+
+########################################
+# Dinamica
+
+my @dinamicas = @{ $track_config->{ dinamicas } }; 
+my $cantidad_dinamicas = scalar @dinamicas;
 
 
 ########################################
 # Secuencia de alturas ( @altura[ n ] )
 
-my @A    = ( "5 -12", "5 -12", "6 -12", "5 -12", 1, "7 -12" ); 
-my @Ap   = ( "5 -12", "5 -12", "6 -12", "5 -12", 2, 1 ); 
-my @App  = ( "5 -12", "5 -12", 5, 3, 1, 1, "7 -12", "6 -12" ); 
-my @Appp = ( 4, 4, 3, 1, 2, 1 ); 
+my @motivios = @{ $track_config->{ secuencia } };
+my $repeticiones = $track_config->{ repeticiones };
+
 my @secuencia;  
-push @secuencia, @A, @Ap, @App, @Appp;
+foreach ( @motivios ){
+    push @secuencia,  @{ $_->{ orden } };
+}
+push @secuencia, ( @secuencia ) x $repeticiones;
 
 
 ########################################
-# Melodia
+# Construir 
 
-# Midi Setup
+# Setup
 my @events = (
-    ['set_tempo', 0, 1000_000], # 1qn = 1000 miliseconds
+    [ 'set_tempo', 0, $pulso ], # 1qn = 1000 miliseconds
+    [ 'patch_change', 1, 1, $canal ], # 1qn = 1000 miliseconds
 );
 
 my $index = 0;
@@ -62,16 +77,14 @@ foreach (
     my ( 
         $delta, # posicion en las lista de alturas
 	$transpo
-    ) = split ;
-    say $delta;
-
+    ) = split;
     my $altura = @alturas[ ( $delta - 1) % $cantidad_alturas ] + $transpo;
     my $duracion = 96 * @duraciones[ $index % $cantidad_duraciones ] ;
-
+    my $dinamica = 127 * @dinamicas[ $index % $cantidad_dinamicas ] ;
+    my $variacion = .5 + rand ( .3 ); # variacion y compresion dinamica 
     push @events,
-        ['note_on' , 0, 1, $altura, 127],
-	['note_off', $duracion, 1, $altura, 127],
-    ;
+        [ 'note_on' ,  $retardo, $canal, $altura, $dinamica * $variacion ],
+        [ 'note_off', $duracion, $canal, $altura, 1 ];
     $index++;
 }
 
@@ -85,4 +98,5 @@ my $opus = MIDI::Opus->new({
     'tracks' => [ $piano_track ] 
 });
 
-$opus->write_to_file( 'melody.mid' );
+$opus->write_to_file( 'output/melody.mid' );
+
