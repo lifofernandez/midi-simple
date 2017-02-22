d011321e77059dedc1b1cedcd168b962a227f494
lifofernandez <lifofernandez@gmail.com>
2017-02-22 18:22:57 +0100

    agrego motivos

---
+++ melodias.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/env perl
+
+########################################
+# Secuenciador Motivocentrico 
+# 
+# Lisandro Fernández ( Febrero 2017 )
+
+use feature 'say';
+use strict;
+use Data::Dumper;
+use MIDI;
+use YAML::XS 'LoadFile';
+
+########################################
+# General setup
+
+my $pulso = 600_000; # mili
+my $tic = 240; 
+my @confs = ( 
+	# 'tracks/drums.yml',
+	# 'tracks/cymbals.yml',
+	'tracks/bajo.yml',
+);
+my @tracks;
+
+
+foreach ( @confs ){
+    
+    ########################################
+    # Load track from YAML
+    my $conf = LoadFile( $_ );
+    
+    # Track setup
+    my $nombre = $conf->{ nombre };
+    my $canal = $conf->{ canal };
+    my $programa = $conf->{ programa };
+    
+    
+    ########################################
+    # Configuraciones generales
+
+    my $tonica = $conf->{ tonica };
+    my $octava = $conf->{ octava };
+
+    my @escala = map {
+        eval $_ # Perl Ranges suport 
+    } @{ $conf->{ escala }{ notas } };
+
+    my @alturas = map { 
+        $_ + $tonica + ( 12 * $octava ) 
+    } @escala;
+    my $cantidad_alturas = scalar @escala;
+    
+    my @duraciones = @{ $conf->{ duraciones } };
+    my $cantidad_duraciones = scalar @duraciones;
+    my $retraso = $conf->{ retraso }; 
+    # To do: superposicion de las notas
+    
+    my @dinamicas = @{ $conf->{ dinamicas } }; 
+    my $cantidad_dinamicas = scalar @dinamicas;
+    my $piso = $conf->{ piso }; 
+    my $variacion = $conf->{ variacion }; 
+    
+    
+    ########################################
+    # Manipular y definir motivos 
+
+    my %motivos; 
+    foreach( 
+     	# reverse
+     	@{ $conf->{ motivos } }
+    ){
+	my %temp = %{ $_ };
+
+	my $this_id = %temp{ "identificador" }; 
+	
+	# Sobrescribir las configuraciones generales
+	# con las de este motivo, si las hay.
+	my $this_tonica = %temp{ "tonica" } ? %temp{ "tonica" } : $tonica;
+        my $this_octava = %temp{ "octava" } ? %temp{ "octava" } : $octava;
+        
+        my @this_escala;
+        if( %temp{ "escala" } ){
+            @this_escala = map {
+                eval $_ # así soporta rangos en los yamls
+            } @{ %temp{ "escala" }->{ "notas" } };
+	} else {
+            @this_escala = @escala;
+	}
+        my @this_alturas = map { 
+            $_ + $this_tonica + ( 12 * $this_octava ) 
+        } @this_escala;
+
+	my @this_duraciones = %temp{ "duraciones"} ? %temp{ "duraciones" } : @duraciones;
+
+	my @this_dinamicas = %temp{ "dinamicas" }  ? %temp{ "dinamicas" } : @dinamicas;
+	my $this_piso = %temp{ "piso" } ? %temp{ "piso" } : $piso;
+	my $this_variacion = %temp{ "variacion" } ? %temp{ "variacion" } : $variacion;
+        
+	# Construir componentes ( altura, duración y dinámica )
+        my @componentes;
+	foreach( @{ %temp{ "orden" } } ){
+             my ( 
+                  $lector_altura, # posicion en las lista de alturas
+         	  $ajuste
+             ) = split;
+             my $nota_altura = @this_alturas[ ( $lector_altura - 1 ) % scalar @this_alturas ] + $ajuste;
+
+	     my %componente;
+	     $componente{ "altura" } = $nota_altura;
+	     $componente{ "duracion" } =  1; 
+	     $componente{ "dinamica" } = .5;
+	     
+	     push @componentes, %componente;
+        }	
+	$motivos{ $this_id }->{ "componentes" } = @componentes; 
+
+	# push @motivos, %motivo ;
+    }
+    
+
+    ########################################
+    # Poner motivos en secuencia ( array de motivos ) 
+    # nota: Add super especial feture: control de  gap/superposicion entre motivos
+    
+    my $repeticiones = 
+
+    my @secuencia;  
+    foreach( @{ $conf->{ secuencia_motivica } } ){
+        say $_
+	push @secuencia, 
+    }
+    # push @secuencia, ( @secuencia ) x $conf->{ repeticiones };
+    
+    ########################################
+    # Convertir Secuencia a MIDI::Events 
+    
+    # Setup
+    my @events = (
+        [ 'set_tempo', 0, $pulso ], 
+        [ 'text_event', 0, "Track: " . $nombre ], 
+        [ 'patch_change', 0, $canal, $programa ], 
+    );
+    
+    my $index = 0;
+    # foreach ( 
+    # 	# reverse
+    # 	@secuencia 
+    # ){
+    #     my ( 
+    #         $delta, # posicion en las lista de alturas
+    # 	      $transpo
+    #     ) = split;
+
+    #     # Toda esta opereta la voy a pasr al motivo
+    #     my $altura = @escala[ ( $delta - 1 ) % $cantidad_alturas ] + $transpo;
+    #     
+    #     my $duracion   = $tic * @duraciones[ $index % $cantidad_duraciones ];
+    #     my $dinamica   = 127 * @dinamicas[ $index % $cantidad_dinamicas ];
+    #     my $compresion = $piso + rand ( $variacion );
+    #     
+    #     my $inicio = $retraso;
+    #     my $final  = $duracion;
+
+    #     push @events, (
+    #         [ 'note_on' , $inicio, $canal, $altura, $dinamica * $compresion ],
+    #         [ 'note_off', $final,  $canal, $altura, 0 ]
+    #     );
+    #     $index++;
+    # }
+     
+    my $track = MIDI::Track->new({ 
+        'events' => \@events 
+    });
+    push @tracks, $track;
+
+}
+
+my $opus = MIDI::Opus->new({
+    'format' => 1, 
+    'ticks' => $tic,
+    'tracks' => \@tracks 
+});
+$opus->write_to_file( 'output/secuencia.mid' );
+
