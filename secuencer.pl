1c1c2bde5395312644ec293c2bb3a7e4fb2b2bf4
Lisandro Fernández <lifofernandez@gmail.com>
2017-04-04 17:13:00 -0300

    agregando config globales al motivos

--- secuencer.pl
+++ secuencer.pl
@@ -59,96 +59,96 @@ foreach ( @configs ){
 
 
     ########################################
-    # Procesasar y definir motivos
+    # Cargar Motivos
+    # agregar config generales al motivo
 
     my %MOTIVOS= ();
     for(
         @{ $config_file->{ motivos } }
     ){
         my %motivo = %{ $_ };
-        # dump(%motivo);
         my $motivo_ID = $motivo{ ID };
         say " " . $motivo_ID;
-        # Componer motivo
-        # sidd motivo tiene propiedad sobreescribir
+
         for my $prop_global (keys %conf_global){
             if ( !$motivo{ $prop_global } ){
                 my $valor_global = $conf_global{ $prop_global };
-                #say "  agrego: ". $prop_global." : ".$valor_global;
                 $motivo{$prop_global} = $valor_global;
             }
         }
         $MOTIVOS{ $motivo_ID } = \%motivo;
-
-        ## Sobrescribir las configuraciones generales
-        ## con las de este motivo, si las hay.
-        #my $this_tonica = %temp{ "tonica" } ? %temp{ "tonica" } : $tonica;
-        #my $this_octava = %temp{ "octava" } ? %temp{ "octava" } : $octava;
-
-        #my @this_escala;
-        #if( %temp{ "alturas" } ){
-        #    @this_escala = map {
-        #        eval $_ # así soporta rangos en los yamls
-        #    } @{ %temp{ "alturas" } };
-        #} else {
-        #    @this_escala = @escala;
-        #}
-
-        #my @this_alturas = map { 
-        #    $_ + $this_tonica + ( 12 * $this_octava ) 
-        #} @this_escala;
-
-        ## my @this_duraciones = %temp{ "duraciones"} ? %temp{ "duraciones" } : @duraciones;
-        ## my @this_dinamicas = %temp{ "dinamicas" }  ? %temp{ "dinamicas" } : @dinamicas;
-        ## my $this_piso = %temp{ "piso" } ? %temp{ "piso" } : $piso;
-        ## my $this_variacion = %temp{ "variacion" } ? %temp{ "variacion" } : $variacion;
-
-        #########################################
-        ## Procesasar y dwdefinir componentes 
-        ## Componentes = "nota" del motivo combinancion entre: orden, altura, duración y dinámica)
-        #my $n = 1;
-        #my @temp_comps = ();
-        #for( @{ %temp{ "progresion" } } ){
-        #    my (
-        #        $lector_altura, # posicion en las lista de alturas
-        #        $transpo,
-        #        #$repetir,
-        #    ) = split;
-
-        #     my $nota_altura = @this_alturas[ ( $lector_altura - 1 ) % @this_alturas  ] + $transpo;
-        #     # add note repetition suport "while repetir...."
-
-        #     #my $duracion   = $tic * @duraciones[ $index % $cantidad_duraciones ];
-        #     #my $dinamica   = 127 * @dinamicas[ $index % $cantidad_dinamicas ];
-        #     # add silense suport
-        #     # my $compresion = $piso + rand ( $variacion );
-        #     #
-        #     #my $inicio = $retraso;
-        #     #my $final  = $duracion;
-
-        #     my $componente = { 
-        #        indice   => $n,
-        #        altura   => $nota_altura,
-        #        duracion => 1,
-        #        dinamica => .5,
-        #     };
-        #     push @temp_comps, $componente;
-        #     $n++;
-        #}
-
-
-        # my %componentes; 
-        #@componentes{ @temp_comps } = @temp_comps;
-        #$motivos{ $motivo_ID}{"componentes"}= \%componentes; 
     }
+    dump(%MOTIVOS);
 
-    print Dumper(%MOTIVOS->{b});
+    ########################################
+    # Procesar motivos
+    # a partir de sus propiedades componer "notas"
+    # combinado parametros (altura, duracion, dinamicas, etc)
+
+    #my $this_tonica = %temp{ "tonica" } ? %temp{ "tonica" } : $tonica;
+    #my $this_octava = %temp{ "octava" } ? %temp{ "octava" } : $octava;
+
+    #my @this_escala;
+    #if( %temp{ "alturas" } ){
+    #    @this_escala = map {
+    #        eval $_ # así soporta rangos en los yamls
+    #    } @{ %temp{ "alturas" } };
+    #} else {
+    #    @this_escala = @escala;
+    #}
+
+    #my @this_alturas = map { 
+    #    $_ + $this_tonica + ( 12 * $this_octava ) 
+    #} @this_escala;
+
+    ## my @this_duraciones = %temp{ "duraciones"} ? %temp{ "duraciones" } : @duraciones;
+    ## my @this_dinamicas = %temp{ "dinamicas" }  ? %temp{ "dinamicas" } : @dinamicas;
+    ## my $this_piso = %temp{ "piso" } ? %temp{ "piso" } : $piso;
+    ## my $this_variacion = %temp{ "variacion" } ? %temp{ "variacion" } : $variacion;
+
+    #########################################
+    ## Procesasar y dwdefinir componentes 
+    ## Componentes = "nota" del motivo combinancion entre: orden, altura, duración y dinámica)
+    #my $n = 1;
+    #my @temp_comps = ();
+    #for( @{ %temp{ "progresion" } } ){
+    #    my (
+    #        $lector_altura, # posicion en las lista de alturas
+    #        $transpo,
+    #        #$repetir,
+    #    ) = split;
+
+    #     my $nota_altura = @this_alturas[ ( $lector_altura - 1 ) % @this_alturas  ] + $transpo;
+    #     # add note repetition suport "while repetir...."
+
+    #     #my $duracion   = $tic * @duraciones[ $index % $cantidad_duraciones ];
+    #     #my $dinamica   = 127 * @dinamicas[ $index % $cantidad_dinamicas ];
+    #     # add silense suport
+    #     # my $compresion = $piso + rand ( $variacion );
+    #     #
+    #     #my $inicio = $retraso;
+    #     #my $final  = $duracion;
+
+    #     my $componente = { 
+    #        indice   => $n,
+    #        altura   => $nota_altura,
+    #        duracion => 1,
+    #        dinamica => .5,
+    #     };
+    #     push @temp_comps, $componente;
+    #     $n++;
+    #}
+
+
+    # my %componentes; 
+    # @componentes{ @temp_comps } = @temp_comps;
+    # $motivos{ $motivo_ID}{"componentes"}= \%componentes; 
+
 
     ########################################
     # Secuenciar motivos ( array de motivos ) 
     # nota: Add super especial feture: control de  gap/overlap motivos
     # to do: agregar repticiones de secuencia
-    ########################################
 
     # Track setup
     # my @events = (
