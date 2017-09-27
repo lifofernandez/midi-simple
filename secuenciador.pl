8ae8e517a5c3cd14214524684c37d8ca4500c8c4
lifofernandez <lifofernandez@gmail.com>
2017-09-27 15:31:42 -0300

    terminologia

--- secuenciador.pl
+++ secuenciador.pl
@@ -25,20 +25,20 @@ use Data::Dumper;
 
 =head1 ARGUMENTS
 
- --entradas   Multiples Tracks en formato YAML acepta archivos o carpetas.
- --salida     Archivo .mid a generar.
+ --intput     Multiples Tracks en formato YAML acepta archivos o carpetas.
+ --output     Archivo .mid a generar.
  --bpm        Pulsos por minuto para la secuencia.
  --help       Imprime esta ayuda en vez de generar secuencia MIDI.
  --man        Imprime la pagina man completa en vez de generar MIDI.
 
  Los argumentos pueden declararse tanto en forma larga como corta.
  Por ejemplo:
-   secuenciador.pl --entradas ejemplos
-   secuenciador.pl -e ejemplos -e feliz_cumpleanios/melodia.yml
+   secuenciador.pl --intput ejemplos
+   secuenciador.pl -i ejemplos -o secuencia.mid
 
 =head1 OPTIONS
 
- --info       Informacion sobre, modulos, entorno y programa.
+ --sistema    Informacion sobrei entorno, modulos, y programa.
  --verbose    Expone los elementos previo a secuenciarlos. 
 =cut
 
@@ -54,13 +54,13 @@ my (
 my $bpm = '60';
 my @entradas = ();
 GetOptions(
-    'entradas=s' => \@entradas,
-    'salida=s'   => \$salida, 
+    'intput=s'   => \@entradas,
+    'output=s'   => \$salida, 
     'bpm=i'      => \$bpm, 
     'verbose+'   => \$verbose, 
     'help!'      => \$help, 
     'man!'       => \$man,
-    'info!'      => \$info,
+    'sistema!'      => \$info,
 );
 # pod2usage(-verbose => 1) && exit if ($opt_debug !~ /^[01]$/);
 pod2usage( -verbose => 1 ) && exit if defined $help;
@@ -164,12 +164,12 @@ for( @CONFIGS ){
             # combinado parametros ( altura, duracion, dinamicas, etc )
             my @alturas = map {
                   $_ +
-                  $motivo{ alturas }{ tonica } +
-                  ( 12 * $motivo{ alturas }{ octava } )
-            } @{ $motivo{ alturas }{ procesas } };
+                  $motivo{ tonica } +
+                  ( 12 * $motivo{ octava } )
+            } @{ $motivo{ intervalos }{ procesas } };
 
             my $transponer = 0; 
-            $transponer = $motivo{ alturas }{ transponer } if $motivo{ alturas }{ transponer };
+            $transponer = $motivo{ transponer } if $motivo{ transponer };
             my @microforma =  @{ $motivo{ microforma } } ;
             @microforma = reverse @microforma if $motivo{ revertir_microforma };
             my $repetir_motivo =   $motivo{ repetir } // 1;
@@ -192,7 +192,7 @@ for( @CONFIGS ){
 	     }
 
             for( ( @microforma ) x $repetir_motivo ){
-               # posicion en set de alturas
+               # posicion en set de intervalos
                my $cabezal = $_ - 1; 
                # reservar un elemento ( 0 ) para representar  silencio.
                # esto es solo para poder ordenar por altura, 
@@ -201,7 +201,7 @@ for( @CONFIGS ){
                my @VOCES = ();
                for( @{ $motivo{ voces }{ procesas } } ){
                     if ( $_ ne 0 ){
-                        # posicion en en set de alturas para la esta voz 
+                        # posicion en en set de intervalos para la esta voz 
                         my $cabezal_voz = ( $cabezal + $_ + $transponer ) - 1;
                         my $voz = @alturas[ $cabezal_voz % scalar @alturas ];
                         push @VOCES, $voz;
@@ -430,7 +430,8 @@ END{
  tienen ciertas propiedades que son tratadas iguales en todos: set, operador, grano y
  revertir). Soportan rangos y operaciones matematicas (necesita explicacion)
  Ciertas propiedades son particulares de cada elemento:
- Para las alturas, la referencia al centro tonal esta declarada con la propiedad "tonica" y
+
+ Para los intervalos, la referencia esta declarada con la propiedad "tonica" y
  y podemos mover todo el set con la propiedad octava.
  Las duraciones pueden ser acotadas usando las propiedades recorte y retraso   
  Las dinamicas pueden ser "humanizadas" usando la propiedad fluctuacion.
@@ -454,11 +455,11 @@ END{
    canal      : 1 
    programa   : 1
    ordenador  : indice
-   alturas:
+   octava     : 0  
+   tonica     : 60 # C central
+   intervalos:
      set         : [ 0, 2, 4, 5, 7, 9, 11,
                     12, 2+12, 4+12, 5+12, 7+12, 9+12, 11+12 ] # Diatonica Mayor 
-     octava      : 0  
-     tonica      : 60 # C central
    voces:
      set         : [ 1 ] 
      # set         : [ 5, 8, 10 ]  # armonizacion diatonica 6/4 
@@ -514,7 +515,7 @@ END{
  con la propiedad "revertir" con un valor como true o 1 
  En el caso de los motivos, se revierte el orden de los componentes. 
  Para revertir la microforma en si, el orden de la lista de posiciones en el set
- de alturas, usar la propiedad revertir_microforma;
+ de intervalos, usar la propiedad revertir_microforma;
 
 =head1 AUTHOR
 
@@ -541,7 +542,7 @@ END{
 =head1 TODO
 
  Agregar soporte para cambio de:  metro, chanel y programa entre MOTIVOS
- Revisar ordenador 'alturas' y  propiedad altura de los componentes
+ Revisar ordenador 'intervalos' y  propiedad altura de los componentes
  Control de superposicion o separacion entre Estructuras, Motivos y Componentes.
  Lista defacto general para todos los tracks/configs.
  Terminar esta documnetacion.
